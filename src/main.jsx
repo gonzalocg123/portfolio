b8dfaaeb87d077ee20e6206f02eab93a1935292d
Gonzalo Chica Godino <chicagodinogonzalo@gmail.com>
2026-01-31 18:40:44 +0100

    feat: integrate Tailwind CSS via Vite plugin

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import './styles/variables.css'
 import './styles/base.css'
 
 createRoot(document.getElementById('root')).render(
